dfde60a42ba699506bfef8c06e73dc611b4a2c13
bunny <67633271+c-bunny@users.noreply.github.com>
2021-04-15 19:37:34 +0530

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -576,10 +576,7 @@ else if (config.WORKTYPE == 'public') {
     
         var buffer = await googleTTS.synthesize({
             text: ttsMessage,
-            voice: {
-                languageCode: "en-IN",
-                name: "en-IN-Wavenet-B"
-            }
+            voice: 'en-IN'
         });
         await message.client.sendMessage(message.jid,buffer, MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: true});
     }));
